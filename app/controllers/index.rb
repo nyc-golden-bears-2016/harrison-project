5f6182120b9b88a7ca849cf189b54a0b02f6a654
Harrison Zandri <email": null>
2016-07-29 17:26:37 -0400

    support trump

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,24 @@
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key    = "GFuRhtnpK9u12ZNrYxEHbetjC"
+  config.consumer_secret = "DMJ9FZqz4NMOi4SIfNBVgqyYv4Fksex3gmRhfOqO7Wk6DSs8J5"
+end
+
+def client.get_latest_tweet(username)
+  options = {count: 1, include_rts: true}
+  user_timeline(username, options)[0].text
+end
+
+
 get "/" do
   erb :game
+end
+
+post "/tweets" do
+  if request.xhr?
+    tweet = client.get_latest_tweet(params[:username])
+    profile_image = client.user("realDonaldTrump").profile_image_uri
+    return {dialogue: tweet, portrait: profile_image}.to_json
+  else
+    halt(403, "Nothing to see here.")
+  end
 end
--- public/js/game.js
+++ public/js/game.js
@@ -59,7 +59,7 @@ var p1 = new Player();
 
 // Field
 
-var Field = function(x_dimension, y_dimension){
+var Field = function Field(x_dimension, y_dimension){
 	this.y_dimension = y_dimension;
 	this.x_dimension = x_dimension;
 	this.fieldElements = "";
@@ -73,13 +73,27 @@ var Field = function(x_dimension, y_dimension){
 	}
 }
 
-Field.prototype.drawField = function(player){
+Field.prototype.checkPlayerOrNpc = function(player, npc, cur_field, x, y){
+	if ( x === player.pos_x && y === player.pos_y ){
+		cur_field.fieldElements += '<div class="p1"></div>';
+		return true
+	} else{
+		for (var i = npc.length - 1; i >= 0; i--) {
+			if ( x === npc[i].pos_x && y === npc[i].pos_y ){
+				cur_field.fieldElements += '<div class="npc"></div>';
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+Field.prototype.drawField = function(player, npc){
 	this.fieldElements = "";
 	for (var x = this.grid.length - 1; x >= 0; x--) {
 		for (var y = this.grid[x].length - 1; y >= 0; y--) {
-			if ( x === player.pos_x && y === player.pos_y ){
-				this.fieldElements += '<div class="p1"></div>';
-			} else {
+			if (this.checkPlayerOrNpc(player, npc, this, x, y)){}
+			else {
 				this.fieldElements += ('<div class="' + String(this.grid[x][y]) + '"></div>');
 			}
 		}
@@ -88,19 +102,22 @@ Field.prototype.drawField = function(player){
 }
 
 Field.prototype.checkMoemon = function(player, cur_field){
-	var moemonCode = Math.floor(Math.random() * 10);
+	var moemonCode = Math.floor(Math.random() * 15);
 	if ( moemonCode >= 0 && moemonCode <= 3 )
 	{
 		return "Konata";
 	} else if ( moemonCode >= 4 && moemonCode <= 6 )
 	{
 		return "Homura";
-	} else if ( moemonCode >= 7 && moemonCode <= 8 )
+	} else if ( moemonCode >= 7 && moemonCode <= 10 )
 	{
 		return "Nanoha";
+	} else if ( moemonCode >= 11 && moemonCode <= 13 )
+	{
+		return "Mahoro";
 	}
 	else {
-		return "Mahoro";
+		return "HomuShotgun";
 	}
 }
 
@@ -118,9 +135,41 @@ console.log(field.grid.length);
 console.log(field.grid[1].length);
 field.grid[15][15] = "wall";
 
+// Npc
+
+var Npc = function Npc(){
+	this.name = "unknown";
+	this.twitterAccount = "unknown";
+	this.moemon = "DatBoi";
+	this.pos_x = 1;
+	this.pos_y = 1;
+}
+
+Npc.prototype.init = function(name){
+	if (name === "Trump"){
+		this.name = "Trump";
+		this.twitterAccount = "realDonaldTrump";
+		this.moemon = "KonaWaifu";
+		this.pos_x = 10;
+		this.pos_y = 5;
+	} else if (name === "SEGA"){
+		this.name = "SEGA";
+		this.twitterAccount = "SEGA";
+		this.moemon = "Neptune";
+		this.pos_x = 15;
+		this.pos_y = 6;
+	}
+}
+
+var trump = new Npc();
+trump.init("Trump");
+var sega = new Npc();
+sega.init("SEGA");
+var the_npcs = [trump, sega];
+
 // Battle
 
-var Battle = function(){
+var Battle = function Battle(){
 	this.battleElements = "";
 	this.topPos = 0;
 	this.bottomPos = 500;
@@ -237,11 +286,13 @@ var pChar = new PlayerChar();
 var Game = function Game(){
 	this.inField = false;
 	this.inBattle = false;
+	this.inDialogue = false;
 }
 
 Game.prototype.doBattle = function(opponent){
 	this.inBattle = true;
 	this.inField = false;
+	this.inDialogue = false;
 
 	if ( opponent.name === "DatBoi" ){
 		opponent.maxHP = opponent.curHP = 20;
@@ -262,6 +313,14 @@ Game.prototype.doBattle = function(opponent){
 		opponent.maxHP = opponent.curHP = 30;
 		opponent.power = 2;
 		opponent.attackDelay = 13;
+	} else if ( opponent.name === "KonaWaifu"){
+		opponent.maxHP = opponent.curHP = 120;
+		opponent.power = 4;
+		opponent.attackDelay = 5;
+	} else if ( opponent.name === "HomuShotgun"){
+		opponent.maxHP = opponent.curHP = 50;
+		opponent.power = 3;
+		opponent.attackDelay = 6;
 	}
 	else {
 		opponent.maxHP = opponent.curHP = 20;
@@ -272,31 +331,95 @@ Game.prototype.doBattle = function(opponent){
 Game.prototype.doField = function(){
 	this.inBattle = false;
 	this.inField = true;
+	this.inDialogue = false;
+}
+
+Game.prototype.doDialogue = function(npc_name){
+	this.inBattle = false;
+	this.inField = false;
+	this.inDialogue = npc_name;
+}
+
+Game.prototype.checkNpc = function(player, npcs){
+	for (var i = npcs.length - 1; i >= 0; i--) {
+		if (npcs[i].pos_x === player.pos_x && npcs[i].pos_y === player.pos_y){
+			this.doDialogue(npcs[i].name);
+		}
+	};
 }
 
 var game = new Game();
 game.doField();
 
+// Dialogue
+
+var Dialogue = function Dialogue(){
+	this.dialogueElements = "";
+	this.message = "";
+	this.moemon = "";
+	this.npc_name = "";
+	this.npc_image = "";
+	this.challenge_message = "";
+}
+
+
+Dialogue.prototype.fetchNpcInfo = function(npc){
+	this.npc_name = npc.name;
+	this.moemon = npc.moemon;
+	var that = this;
+	$.ajax({
+		method: "POST",
+		url: '/tweets',
+		data: {username: npc.twitterAccount}
+	}).done(function(npc_data){
+		var npc_info = JSON.parse(npc_data);
+		that.message = npc_info.dialogue;
+		that.npc_image = npc_info.portrait.scheme + "://" + npc_info.portrait.host + npc_info.portrait.path;
+	});
+}
+
+Dialogue.prototype.init = function(npc_name){
+	if (npc_name === "Trump"){
+		this.moemon = "KonaWaifu";
+		this.challenge_message = " \"With the glorious power of my waifu I will destroy you and rule this world.\"";
+	}
+}
+
+Dialogue.prototype.drawDialogue = function(){
+	this.dialogueElements = "";
+	this.dialogueElements += '<p class="npc_name">' + this.npc_name + '</p><div style="width: 50px; height: 50px; display: block;"><img style="position: fixed; left: 100px;" src="' + this.npc_image + '"height="50" width="50"></img></div><p class="dialogue">' + (this.message + this.challenge_message) + '</p><p class="challenge">Will you engage in a moemon battle with Trump?</p>';
+}
+
+var dialogue = new Dialogue();
+dialogue.init("Trump");
+dialogue.fetchNpcInfo(trump);
+
 $(document).ready(function(){
-	field.drawField(p1);
+	field.drawField(p1, the_npcs);
 	$('body').html(field.fieldElements);
 	var displayPosition = function(){
+		game.checkNpc(p1, the_npcs);
 		if (game.inField){
-			field.drawField(p1);
+			field.drawField(p1, the_npcs);
 			$('body').html(field.fieldElements);
-		}
-		else if (game.inBattle){
+		} else if (game.inBattle){
 			if (battle.battleReady()){
 				opponent.attack(pChar);
 				if (opponent.isDead()){
 					battle.cleanUp();
 					game.doField();
 				}
-			}else {
+				if (pChar.isDead()){
+					throw "You died!";
+				}
+			} else {
 				battle.positionElements();
 			}
 			battle.drawBattle(pChar, opponent);
 			$('body').html(battle.battleElements);
+		} else if (game.inDialogue) {
+			dialogue.drawDialogue();
+			$('body').html(dialogue.dialogueElements);
 		}
 	}
 	setInterval(displayPosition, 33);
@@ -317,8 +440,19 @@ $(document).ready(function(){
 				game.doBattle(opponent);
 			}
 		} else if ( game.inBattle && battle.battleReady() ){
-			if (e.keyCode === 38){
+			if (e.keyCode === 66){
 				pChar.attack(opponent);
+			}
+		} else if ( game.inDialogue ){
+			if (e.keyCode === 89){
+				//accept
+				p1.pos_x--;
+				opponent.name = dialogue.moemon
+				game.doBattle(opponent);
+			} else if (e.keyCode === 78){
+				//decline
+				p1.pos_x--;
+				game.doField();
 			}
 		}
 	});
